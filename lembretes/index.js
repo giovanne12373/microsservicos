15d8f210b9f73756166e0659c6cec3bcadcedd99
asbonato <asbonato@gmail.com>
2022-05-14 19:20:10 -0300

    endpoints de eventos bc 4.3.15

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,3 +1,4 @@
+const axios = require ('axios');
 const express = require ('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -9,14 +10,26 @@ app.get('/lembretes', (req, res) => {
     res.send(lembretes);
 });
 
-app.post('/lembretes', (req, res) => {
+app.post('/lembretes', async (req, res) => {
     contador ++;
     const {texto} = req.body;
     lembretes[contador] = {
         contador, texto
     }
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "LembreteCriado",
+        dados: {
+            contador,
+            texto,
+        },
+    });
     res.status(200).send(lembretes[contador]);
 });
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({msg: "ok"});
+})
 
 app.listen(4000, () => {
     console.log('Lembretes. Porta 4000')
